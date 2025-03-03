6f448590ef5970b697d94918cc769fcf35f5dec0
BWZ-CODE <mmige24@gmail.com>
2025-03-03 13:32:26 -0500

    Initial Commit

---
+++ cookieRabbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cookieRabbit
+//
+//  Created by Miguel Angel Bohorquez on 3/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cookieRabbit/cookieRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookieRabbitApp.swift
+//  cookieRabbit
+//
+//  Created by Miguel Angel Bohorquez on 3/03/25.
+//
+
+import SwiftUI
+
+@main
+struct cookieRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
